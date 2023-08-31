a630ed1e5c532825554b17224bdc8a586a16c192
Eric Ndungutse <dav.ndungutse@gmail.com>
2023-09-01 00:41:59 +0300

    read report from data db: video 1:07

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Delete, Get, Param, Post, Put } from "@nestjs/common/decorators";
+import { data, ReportType } from "./data";
+
 
 @Controller('/report/:type')
 export class AppController{
   @Get('')
   getAllReports(@Param('type') type){
-    console.log(type )
-    return [{
-      title:"Shaving"
-    }]
+    const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSE
+    const report = data.report.filter(report=> report.type === reportType)
+    return {
+      data: {
+        count: report.length,
+        report: report
+      }
+    }
   }
 
   @Post()
