24ee44e5d776fd0f6df73b1db50e0ce393131509
Eric Ndungutse <dav.ndungutse@gmail.com>
2023-09-02 22:22:24 +0300

    connect two modules/apps with import and export

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ReportService } from 'src/report/report.service';
+import { ReportType } from 'src/data';
 
 @Injectable()
-export class SummaryService {}
+export class SummaryService {
+    constructor(private readonly reportService: ReportService){}
+    calculateSumary(){
+        const expenses = this.reportService.getAllReports(ReportType.EXPENSE)
+        console.log(expenses)
+        return "summary"
+    }
+}
